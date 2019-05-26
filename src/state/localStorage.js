b2151b2dd10276486bdce3d83777c6d42d93eec0
Narendras <n.syahrasyad.n@gmail.com>
2019-05-26 15:10:49 -0700

    Complete reducers

---
+++ src/state/localStorage.js
@@ -0,0 +1,18 @@
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('store')
+    if (!serializedState) return undefined
+    return JSON.parse(serializedState)
+  } catch (e) {
+    return undefined
+  }
+}
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('store', serializedState)
+  } catch (e) {
+    console.error(e)
+  }
+}
--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,16 @@
 import { createStore, combineReducers } from 'redux'
+import { loadState, saveState } from './localStorage'
 import * as reducers from './ducks'
 
-export default createStore(
+const persistedState = loadState()
+const store = createStore(
   combineReducers(reducers),
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
+
+export default store
